73c19bc0916706715a19b82c46a776bc6a61bcdb
MarsMatthew <89168748+MarsMatthew@users.noreply.github.com>
2022-04-29 08:36:37 -0600

    Add files via upload

--- src/Player.py
+++ src/Player.py
@@ -1,29 +1,19 @@
 class Player:
-    Name = input("Enter Your Name: ")
-
-    HP = 20
-    ATKval = 1
-    ARMval = 0
-    MGCval = 1
-    Money = 0
-    XP = 0
-
-    def __init__(self):
-        self.HP = 20
-        self.ATKval = 1
-        self.ARMval = 0
-        self.MGCval = 1
-        self.Money = 0
-        self.XP = 0
-
-    def MoveFWRD(self):
-
-    def MoveBKWRD(self):
-
-    def ATK(self):
-
-    def MGC(self):
-
-    def Surrender(self):
-
-    def Death(self):
+  Name = input("What is your name:")
+  HP = 50
+  ATK = 5
+  ARM = 0
+  MGC = 0
+  MON = 0
+  XP = 0
+  Alive = 1
+  def __init__(self):
+    self.HP = 50
+    self.ATK = 5
+    self.ARM = 0
+    self.MGC = 0
+    self.MON = 0
+    self.XP = 0
+  #def Inventory(self):
+  #def Personal(self):
+    
